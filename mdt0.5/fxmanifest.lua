81ff7c9213aeb153a392e2a58af0a215531e8ea0
Smirf123 <smirf368@outlook.com>
2020-08-28 21:47:52 -0500

    Migration from _resource.lua to newer fxmanifest

---
+++ mdt0.5/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Hyperz'
+description 'Blast CAD/MDT Integration'
+version '1.0.0'
+
+client_scripts {
+    'main.lua'
+}
+files {
+    'html/index.html',
+   
+    'html/style.css',
+    'html/reset.css',
+    'html/listener.js',
+    "html/script.js"
+}
